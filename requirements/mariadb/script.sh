c5df902d7b63c202fc97eb73c0557e1acd04eaef
ziliolu <luzilio.p@gmail.com>
2024-09-24 08:22:44 -0400

    ssl certificate working well

--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -2,7 +2,6 @@
 
 mysql_install_db
 
-# Substitute placeholders with actual values in init.sql
 sed -i "s/__DB_NAME__/${DB_NAME}/g; s/__DB_USER__/${DB_USER}/g; s/__DB_PASS__/${DB_PASS}/g" /etc/mysql/init.sql
 
 # Keep MariaDB running
